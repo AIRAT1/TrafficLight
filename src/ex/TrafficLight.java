b261f16f26ef0793abaefe4584666791b4681bb4
AIRAT1 <ayrat1@mail.ru>
2017-03-20 16:07:40 +0200

    add try-with-resources in TrafficLight.class

--- src/ex/TrafficLight.java
+++ src/ex/TrafficLight.java
@@ -1,23 +1,27 @@
 package ex;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 
 public class TrafficLight {
     void start() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            try {
-                System.out.println(Constants.GREETING_1);
-                System.out.println(Constants.GREETING_2);
-                String time = reader.readLine();
-                if (time.equals(Constants.EXIT)) return;
-                System.out.println(String.format(Constants.RESULT_TEXT, getColor(Integer.parseInt(time))));
-                System.out.println(Constants.SEPARATOR);
-            } catch (Exception e) {
-                System.out.println(Constants.ERROR_TEXT);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            while (true) {
+                try {
+                    System.out.println(Constants.GREETING_1);
+                    System.out.println(Constants.GREETING_2);
+                    String time = reader.readLine();
+                    if (time.equals(Constants.EXIT)) return;
+                    System.out.println(String.format(Constants.RESULT_TEXT, getColor(Integer.parseInt(time))));
+                    System.out.println(Constants.SEPARATOR);
+                } catch (Exception e) {
+                    System.out.println(Constants.ERROR_TEXT);
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
